aa76561c1572070405bd49c6df73536a929d6d01
basma-skaik <basmahskaik@gmail.com>
2022-11-09 19:12:46 +0200

    films

--- src/screens/HomeScreen/Home.js
+++ src/screens/HomeScreen/Home.js
@@ -4,6 +4,7 @@ import {
   View,
   Keyboard,
   TouchableWithoutFeedback,
+  ScrollView,
 } from 'react-native';
 import {styles} from './styles';
 import AntDesign from 'react-native-vector-icons/AntDesign';
@@ -15,48 +16,50 @@ import MoviesFlatlist from '../../components/MoviesFlatlist';
 
 const Home = () => {
   return (
-    <TouchableWithoutFeedback
-      onPress={() => {
-        Keyboard.dismiss();
-      }}>
-      <View style={{backgroundColor: '#FFFFFF'}}>
-        <View style={styles.Contanier}>
-          <View style={styles.searchContanier}>
-            <AntDesign
-              name="search1"
-              size={26}
-              color={'#6C63FF'}
-              style={{flex: 1, padding: 17}}
-            />
-            <TextInput
-              style={styles.searchTextInput}
-              placeholder="Search for What you want.."
-            />
-            <FontAwesome
-              name="microphone"
-              size={26}
-              color={'#6C63FF'}
-              style={{flex: 1, padding: 17}}
+    <ScrollView>
+      <TouchableWithoutFeedback
+        onPress={() => {
+          Keyboard.dismiss();
+        }}>
+        <View style={{backgroundColor: '#FFFFFF'}}>
+          <View style={styles.Contanier}>
+            <View style={styles.searchContanier}>
+              <AntDesign
+                name="search1"
+                size={20}
+                color={'#6C63FF'}
+                style={{flex: 1, padding: 17}}
+              />
+              <TextInput
+                style={styles.searchTextInput}
+                placeholder="Search for What you want.."
+              />
+              <FontAwesome
+                name="microphone"
+                size={26}
+                color={'#6C63FF'}
+                style={{flex: 1, padding: 17}}
+              />
+            </View>
+            <Entypo
+              name="list"
+              size={35}
+              color={'black'}
+              style={{
+                flex: 1,
+                marginVertical: 27,
+                marginRight: 29,
+              }}
             />
           </View>
-          <Entypo
-            name="list"
-            size={35}
-            color={'black'}
-            style={{
-              flex: 1,
-              marginVertical: 27,
-              marginRight: 29,
-            }}
-          />
+          <OnBoardingImages />
+          <Title title="What's New" />
+          <MoviesFlatlist />
+          <Title title="Popular movies" />
+          <MoviesFlatlist />
         </View>
-        <OnBoardingImages />
-        <Title title="What's New" />
-        <MoviesFlatlist />
-        <Title title="Popular movies" />
-        <MoviesFlatlist />
-      </View>
-    </TouchableWithoutFeedback>
+      </TouchableWithoutFeedback>
+    </ScrollView>
   );
 };
 
